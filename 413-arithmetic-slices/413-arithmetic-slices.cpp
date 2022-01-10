5b9f286de6ea1da5b648247c3a33584b1cd89b7c
code-gene <72328725+code-gene@users.noreply.github.com>
2022-01-10 08:00:45 +0530

    Time: 9 ms (5.13%), Space: 7.4 MB (15.69%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) 
+    {
+        int n = nums.size(), cnt = 0;
+        vector<int> vec(n, 0);
+        
+        for(int i = 2; i < n; i++)
+        {
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                vec[i] = 1;
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 2; i < n; i++) 
+        {
+            // cout << vec[i] << " ";
+            if(vec[i] == 1) cnt++;
+            else
+            {
+                ans += cnt * (cnt+1) / 2;
+                cnt = 0;
+            }
+        }
+        
+        ans += cnt * (cnt+1) / 2;
+        return ans;
+    }
+};
+
+/*
+
+1 2 3 4 5
+
+
+*/
