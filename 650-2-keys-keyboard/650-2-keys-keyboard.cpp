8cd161d008a0754ae1551fa18d05d78caed6923f
code-gene <72328725+code-gene@users.noreply.github.com>
2022-01-18 22:08:20 +0530

    Time: 1890 ms (5.08%), Space: 50.2 MB (7.89%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[1005][10005];
+    int F(int i, int k, int n)
+    {
+        if(i == n) return 0;
+        if(i > n) return 100000;
+        
+        if(dp[i][k] != -1) return dp[i][k];
+        
+        return dp[i][k] = min(1 + F(i+k, k, n), 2 + F(2*i, i, n));
+    }
+    int minSteps(int n) 
+    {
+        int i = 2, k = 1;
+        if(n == 1) return 0;
+        
+        memset(dp, -1, sizeof(dp));
+        
+        return 2 + F(i, 1, n);
+    }
+};
+
+/*
+
+AAAAAAAA
+
+*/
