2eae57bcf6b3bd5e914260921e3e1265a3a962f2
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2024-08-18 19:23:00 +0530

    Time: 4 ms (60.86%), Space: 8.4 MB (50.77%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,55 +1,26 @@
 class Solution {
 public:
-    int count;
-    bool checkValid(vector<string> &board, int row, int col, int size) 
-    {
-        for(int i = 0; i < row; i++) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
-        }
-        
-        for(int i = 1; i <= row; i++)
-        {
-            if(col-i >= 0) {
-                if(board[row-i][col-i] == 'Q'){
-                    return false;
-                }
-            }
-            if(col+i < size){
-                if(board[row-i][col+i] == 'Q'){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    void helper(vector<string> &board, int row, int size)
-    {
-        if(row == size){
-            count++;
-            return;
-        }
-        for(int i = 0; i<size; i++)
-        {
-            if(checkValid(board, row, i, size)){
-                board[row][i] = 'Q';
-                helper(board, row+1, size);
-                board[row][i] = '.';
-            }
-        }
-    }
     int totalNQueens(int n) 
     {
-        string str;
-        str.append(n, '.');
-        vector<string> board(n, str);
-        
-        count = 0;
-        
-        helper(board, 0, n);
-        return count;
+	    vector<bool> col(n), d1(2*n-1), d2(2*n-1);
+	    return solve(col, d1, d2, 0);
+    }
+   
+    int solve(vector<bool>& col, vector<bool>& d1, vector<bool>& d2, int row) 
+    {
+	    int n = size(col), count = 0;
+        if(row == n) return 1;
+	    
+        for(int column = 0; column < n; column++)
+        {
+            if(!col[column] && !d1[row + column] && !d2[row - column + n - 1])
+            { 
+			    col[column] = d1[row + column] = d2[row - column + n - 1] = true;
+			    count += solve(col, d1, d2, row + 1);
+			    col[column] = d1[row + column] = d2[row - column + n - 1] = false;
+		    }  
+        }
+	    return count;
     }
 };
 
