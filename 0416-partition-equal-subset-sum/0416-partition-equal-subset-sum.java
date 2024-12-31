393f79b5e2c0bbf3a69cd78fbbf7675dac7bbe33
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2024-12-31 06:25:12 +0530

    Time: 40 ms (63.76%), Space: 56.3 MB (39.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,12 +3,12 @@ class Solution {
         if (s == 0 && n == 0) return 1;
         if (s < 0 || n == 0) return 0;
 
-        if (dp[s][n] != -1) return dp[s][n];
+        if (dp[n][s] != -1) return dp[n][s];
 
         if (nums[n-1] <= s) {
-            dp[s][n] = (F(nums, s - nums[n-1], n-1, dp) == 1 || F(nums, s, n-1, dp) == 1) ? 1 : 0;
+            dp[n][s] = (F(nums, s - nums[n-1], n-1, dp) == 1 || F(nums, s, n-1, dp) == 1) ? 1 : 0;
         } else {
-            dp[s][n] = F(nums, s, n-1, dp);
+            dp[n][s] = F(nums, s, n-1, dp);
         }
         return dp[s][n];
     }
@@ -20,14 +20,32 @@ class Solution {
         }
 
         if (s % 2 == 1) return false;
-
         s = s / 2;
-        int[][] dp = new int[s+1][n+1];
+
+        int[][] dp = new int[n+1][s+1];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
+        
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j <= s; j++) {
+            dp[0][j] = 0;
+        }
 
-        return F(nums, s, n, dp) == 1;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= s; j++) 
+            {
+                if(nums[i-1] <= j) {
+                    dp[i][j] = (dp[i-1][j-nums[i-1]] == 1 || dp[i-1][j] == 1) ? 1: 0;
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[n][s] == 1;
     }
 }
 
