ad5b4efaa135fd1d41d3e3da5959efa8f05bd936
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-01-01 04:49:54 +0530

    Time: 847 ms (67.57%), Space: 55.7 MB (95.95%) - LeetHub

--- 1881-closest-subsequence-sum/1881-closest-subsequence-sum.java
+++ 1881-closest-subsequence-sum/1881-closest-subsequence-sum.java
@@ -1,74 +1,65 @@
 class Solution {
-    public int[] subsetSums(int arr[], int n)
-    {
-        long totalSubsets = (1L << n);
-        int[] allPossibleSubsetSums = new int[(int) totalSubsets];
+    public int[] calculateSubsetSums(int[] array, int size) {
+        long totalSubsets = (1L << size);
+        int[] subsetSums = new int[(int) totalSubsets];
 
-        int k = 0;
-        for(int i = 0; i < totalSubsets; i++)
-        {
+        for (int subsetMask = 0; subsetMask < totalSubsets; subsetMask++) {
             int sum = 0;
-            for(int j = 0; j < n; j++)
-            {
-                if((i & (1 << j)) != 0) {
-                    sum += arr[j];
+            for (int bitIndex = 0; bitIndex < size; bitIndex++) {
+                if ((subsetMask & (1 << bitIndex)) != 0) {
+                    sum += array[bitIndex];
                 }
             }
-            allPossibleSubsetSums[k++] = sum;
+            subsetSums[subsetMask] = sum;
         }
-        return allPossibleSubsetSums;
+        return subsetSums;
     }
 
-    public int binarySearchClosest(int[] arr, int num) {
-        int l = 0, r = arr.length - 1;
-        int closest = Integer.MAX_VALUE;
+    public int findClosestDifference(int[] sortedArray, int target) {
+        int left = 0, right = sortedArray.length - 1;
+        int minDifference = Integer.MAX_VALUE;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            closest = Math.min(closest, Math.abs(arr[mid] - num));
-            
-            if(arr[mid] == num) 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            minDifference = Math.min(minDifference, Math.abs(sortedArray[mid] - target));
+
+            if (sortedArray[mid] == target) {
                 return 0;
-            else if (arr[mid] < num) 
-                l = mid + 1;
-            else r = mid - 1;
+            } else if (sortedArray[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
 
-        return closest;
+        return minDifference;
     }
-    
-    public int minAbsDifference(int[] nums, int goal) 
-    {
-        int n = nums.length;
-        int mid = ((n+1)/2);
 
-        int[] left = new int[mid];
-        int[] right = new int[n-mid];
+    public int minAbsDifference(int[] nums, int goal) {
+        int totalSize = nums.length;
+        int leftSize = (totalSize + 1) / 2;
 
-        for(int i = 0; i < mid; i++) {
-            left[i] = nums[i];
-        }   
-        int k = 0;
-        for(int j = mid; j < n; j++) {
-            right[k++] = nums[j];
+        int[] leftHalf = new int[leftSize];
+        int[] rightHalf = new int[totalSize - leftSize];
+
+        for (int i = 0; i < leftSize; i++) {
+            leftHalf[i] = nums[i];
+        }
+        for (int i = leftSize; i < totalSize; i++) {
+            rightHalf[i - leftSize] = nums[i];
         }
 
-        int[] leftSubsetSum = subsetSums(left, mid);
-        int[] rightSubsetSum = subsetSums(right, n-mid);
+        int[] leftSubsetSums = calculateSubsetSums(leftHalf, leftSize);
+        int[] rightSubsetSums = calculateSubsetSums(rightHalf, totalSize - leftSize);
 
-        Arrays.sort(rightSubsetSum);
+        Arrays.sort(rightSubsetSums);
 
-        int i = 0, x = leftSubsetSum.length, y = rightSubsetSum.length;
-        int mn = Integer.MAX_VALUE;
-        while(i < x) 
-        {
-            int ls = leftSubsetSum[i];
-            int closest = binarySearchClosest(rightSubsetSum, goal - ls);
-
-            mn = Math.min(mn, closest);
-            i++;
+        int minDifference = Integer.MAX_VALUE;
+        for (int leftSum : leftSubsetSums) {
+            int closestDiff = findClosestDifference(rightSubsetSums, goal - leftSum);
+            minDifference = Math.min(minDifference, closestDiff);
         }
 
-        return mn;
+        return minDifference;
     }
 }
