23188cba672e12450f47a2a8c9a827117eb21c29
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-02-03 08:02:50 +0530

    Time: 288 ms (5.25%), Space: 46.5 MB (37.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,45 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int water = 0;
-        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < n; i++) 
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        left[0] = height[0];
+        for(int i = 1; i < n; i++)
         {
-            while (!stack.isEmpty() && height[i] > height[stack.peek()]) 
-            {
-                int top = stack.pop();
-                if (stack.isEmpty()) {
-                    break;
-                }
-                int distance = i - stack.peek() - 1;
-                int boundedHeight = Math.min(height[i], height[stack.peek()]) - height[top];
-                water += distance * boundedHeight;
-            }
-            stack.push(i);
+            left[i] = Math.max(left[i-1], height[i]);
+        }
+        
+        right[n-1] = height[n-1];
+        for(int i = n-2; i >=0; i--)
+        {
+            right[i] = Math.max(right[i+1], height[i]);
         }
 
-        return water;
+        for(int i = 0; i < n; i++){
+            System.out.print(left[i] + " ");
+        }
+        System.out.println();
+        for(int i = 0; i < n; i++){
+            System.out.print(right[i] + " ");
+        }
+        System.out.println();
+
+        int trapped_water = 0;
+        for(int i = 1; i < n-1; i++)
+        {
+            trapped_water += Math.min(left[i], right[i]) - height[i];
+        }
+        
+        return trapped_water;    
     }
 }
+
+/*
+
+[0,1,0,2,1,0,1,3,2,1,2,1]
+[0,1,1,2,2,2,2,3,3,3,3,3]
+[3,3,3,3,3,3,3,3,2,2,2,1]
+
+*/
