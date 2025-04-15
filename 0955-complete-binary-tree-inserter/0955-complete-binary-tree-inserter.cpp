d6a5195c599259171d4f72dfa08e274915bf5f4f
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-04-15 11:33:02 +0530

    Time: 47 ms (8.36%), Space: 68.9 MB (19.06%) - LeetHub

---
+++ 0955-complete-binary-tree-inserter/0955-complete-binary-tree-inserter.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class CBTInserter {
+public:
+    TreeNode *root = NULL;
+    CBTInserter(TreeNode* r) {
+        root = r;
+    }
+    
+    int insert(int val) 
+    {
+        queue<TreeNode*> q;
+        cout << root << "\n";
+        q.push(root);
+
+        TreeNode *curr;
+        while(!q.empty()) 
+        {
+            curr = q.front();
+            q.pop();
+
+            if(curr->left == NULL) 
+            {
+                TreeNode *newNode = new TreeNode(val);
+                curr->left = newNode;
+                break;
+            }
+            if(curr->right == NULL) 
+            {
+                TreeNode *newNode = new TreeNode(val);
+                curr->right = newNode;
+                break;
+            }
+            q.push(curr->left);
+            q.push(curr->right);
+        }
+        return curr->val;
+    }
+    
+    TreeNode* get_root() {
+        return root;
+    }
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter* obj = new CBTInserter(root);
+ * int param_1 = obj->insert(val);
+ * TreeNode* param_2 = obj->get_root();
+ */
