5a189d357e61e25caa40466b02472b552f25f30b
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-05-02 15:44:06 +0530

    Time: 0 ms (100%), Space: 21.1 MB (96.71%) - LeetHub

---
+++ 2916-check-if-it-is-possible-to-split-array/2916-check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canSplitArray(vector<int>& nums, int m) {
+        int i = 0, j = 0, n = nums.size();
+        if(n <= 2) return true;
+
+        for(int i = 0;i < n-1; i++) {
+            if(nums[i] + nums[i+1] >= m) return true;
+        }
+
+        return false;
+    }
+};
