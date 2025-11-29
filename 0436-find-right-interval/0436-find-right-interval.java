9227ecc56ab2bcb086560b9a69d3abb14a3e9bc4
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-11-29 22:22:44 +0530

    Time: 26 ms (22.97%), Space: 49.9 MB (52.64%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,36 +1,16 @@
 class Solution {
-    public int binarySearch(ArrayList <int[]> list, int target) {
-        int left = 0, right = list.size() - 1, ans = -1;
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(list.get(mid)[0] == target) {
-                return list.get(mid)[1];
-            } else if(list.get(mid)[0] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-                ans = list.get(mid)[1];
-            }
-        }
-        return ans;
-    }
     public int[] findRightInterval(int[][] intervals) 
     {
-        ArrayList <int[]> list = new ArrayList<>();
         int n = intervals.length;
-        for(int i = 0; i < n; i++) {
-            list.add(new int[] {intervals[i][0], i});
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(intervals[i][0], i);
         }
-        Collections.sort(list, (a, b)->a[0]-b[0]);
+
         int[] ans = new int[n];
-        for(int i = 0; i < n; i++) {
-            int x = intervals[i][0], y = intervals[i][1];
-            int idx = binarySearch(list, y);
-            if(idx == -1) {
-                ans[i] = -1;
-            } else {
-                ans[i] = idx;
-            }
+        for (int i = 0; i < n; i++) {
+            Integer key = map.ceilingKey(intervals[i][1]);
+            ans[i] = (key == null) ? -1 : map.get(key);
         }
         return ans;
     }
