e5af86869d318c2d86765564b7156bbfac4e60a8
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-12-20 10:45:00 +0530

    Time: 92 ms (39.92%), Space: 56.1 MB (31.87%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    bool isPredecessor(string str1, string str2)
+    int longestStrChain(vector<string>& words) 
     {
-        int n1 = str1.length(), n2 = str2.length(); 
-        if(n2 - n1 != 1) return false;
-        int i = 0, j = 0;
-        while(j < n2)
-        {
-            if(str1[i] == str2[j]) {
-                i++; j++;
-            }
-            else j++;
-        } 
-        if(i == n1) return true;
-        return false;
-    }
-    int longestStrChain(vector<string>& words) {
-        int n = words.size(), mx = 1;
-        vector<int> dp(n, 1);
-
-        sort(words.begin(), words.end(), [](const string& a, const string& b) {
-            return a.length() < b.length();
+        unordered_map<string, int> dict;
+        int i, j;
+        int ans = 0;
+        
+        sort(words.begin(), words.end(), [](string &a, string &b){
+            return a.size() < b.size();
         });
-        for(int i = 0; i < n; i++)
+        
+        for (i = 0; i < words.size(); i++) 
         {
-            for(int j = i+1; j < n; j++)
+            int count = 0;
+            for (j = 0; j < words[i].size(); j++) 
             {
-                if(isPredecessor(words[i], words[j])) {
-                    dp[j] = max(dp[j], dp[i] + 1);
-                    mx = max(mx, dp[j]);
+                string tmp = words[i].substr(0, j) + words[i].substr(j + 1);
+                if (tmp != "") {
+                    count = max(count, dict[tmp]);
                 }
             }
+            dict[words[i]] = count + 1;
+            ans = max(ans, count + 1);
         }
-        return mx;
+        return ans;
+        
     }
 };
