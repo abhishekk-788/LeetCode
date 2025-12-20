6a1e35f4b5ba1d5b2c60d21aa9505f262da717c3
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-12-20 11:07:20 +0530

    Time: 4 ms (77.97%), Space: 26.4 MB (94.31%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        int n = pairs.size(), mx = 1;
-        vector<int> dp(n, 1);
-
-        sort(pairs.begin(), pairs.end());
-
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i+1; j < n; j++)
-            {
-                if(pairs[j][0] > pairs[i][1]) {
-                    dp[j] = max(dp[j], dp[i] + 1);
-                    mx = max(mx, dp[j]);
-                }
+        sort(pairs.begin() , pairs.end() , [](const vector<int>& a , const vector<int>& b){
+            return a[1] < b[1];
+        });
+        int prev = INT_MIN, count = 0;
+        for(int i = 0, n = pairs.size(); i < n; ++i){
+            if(pairs[i][0] > prev){
+                ++count;
+                prev = pairs[i][1];
             }
         }
-
-        return mx;
+        return count;
     }
 };
