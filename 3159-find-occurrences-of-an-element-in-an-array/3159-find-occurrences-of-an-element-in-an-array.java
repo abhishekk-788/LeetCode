1e75cc4322507adb0998c69761c453a78e997eb6
Abhishek Kumar <72328725+abhishekk-788@users.noreply.github.com>
2025-12-23 17:33:38 +0530

    Time: 5 ms (89.74%), Space: 173.2 MB (25.37%) - LeetHub

---
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == x) {
+                list.add(i);
+            }
+        }
+
+        int n = list.size();
+        int[] ans = new int[queries.length];
+
+        for(int i = 0; i < queries.length; i++)
+        {
+            if(queries[i] > n) {
+                ans[i] = -1;
+            }
+            else ans[i] = list.get(queries[i]-1);
+        }
+        return ans;
+    }
+}
